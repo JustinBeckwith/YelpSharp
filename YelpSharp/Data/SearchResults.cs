3716d2d371248d310101c654f474d17fcd822af5
Joseph Connolly <joseph.connolly@live.com>
2012-05-11 08:53:24 -0700

    Adding error property to response, double url encode ampersand

--- YelpSharp/Data/SearchResults.cs
+++ YelpSharp/Data/SearchResults.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace YelpSharp.Data
 {
@@ -24,5 +21,10 @@ namespace YelpSharp.Data
         /// Total number of business results
         /// </summary>
         public int total { get; set; }
+
+        /// <summary>
+        /// Error returned by Yelp Api, null if none
+        /// </summary>
+        public SearchError error { get; set; }
     }
 }
