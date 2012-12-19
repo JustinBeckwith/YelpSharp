ea2a0a37d07304c546794c4a3f7fb9f2557b20c1
Justin Beckwith <justin.beckwith@gmail.com>
2012-12-18 20:43:56 -0800

    nuget package restore on samples

---
+++ Samples/MvcSample/Backup/MvcSample/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+using MvcSample.Models;
+
+using YelpSharp;
+using YelpSharp.Data;
+
+namespace MvcSample.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class HomeController : Controller
+    {
+        //--------------------------------------------------------------------------
+        //
+        //	Action Methods
+        //
+        //--------------------------------------------------------------------------
+
+        #region Index
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            //ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+        #endregion
+
+
+        #region Results
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public ActionResult Results(SimpleSearchModel model)
+        {
+            Yelp yelp = new Yelp(MvcApplication.Options);
+            var results = yelp.Search(model.Term, model.Location);
+            return View(results);
+        }
+
+        #endregion
+
+        #region About
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult About()
+        {
+            return View();
+        }
+        #endregion
+
+
+        
+    }
+}
