89874e48fa4bd4aa7feff83c93723dd981dc6ac4
Justin Beckwith <justin.beckwith@gmail.com>
2012-12-21 17:19:59 -0800

    changed all config to Config.cs file

---
+++ YelpSharpTests/Config.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YelpSharp;
+
+namespace YelpSharpTests
+{
+    class Config
+    {
+        #region GetOptions
+        /// <summary>
+        /// return the oauth options in this case from app.config
+        /// </summary>
+        /// <returns></returns>
+        public static Options Options
+        {
+            get
+            {
+                throw new InvalidOperationException("Please open Config.cs and add your own keys for the YELP API");
+                return new Options()
+                {
+                    AccessToken = "YOUR ACCESS TOKEN HERE",
+                    AccessTokenSecret = "YOUR ACESS TOKEN SECRET HERE",
+                    ConsumerKey = "YOUR CONSUMER KEY HERE",
+                    ConsumerSecret = "YOUR CONSUMER SECRET HERE"
+                };
+            }
+        }
+        #endregion
+    }
+}
